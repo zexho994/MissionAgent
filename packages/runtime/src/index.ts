1cbac9260e8aa3aa128501559896344833b0684d
Zexho.zou <zexho.zou@imile.me>
2026-05-14 17:09:08 +0800

    feat: add skill loading tools

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -2,4 +2,6 @@ export * from "./pi-sdk-adapter.js";
 export * from "./pi-hooks.js";
 export { createWebSearchTool, searchWeb } from "./pi-extensions/web-search.js";
 export type { WebSearchToolOptions, SearchOptions, SearchResult, SearchResponse } from "./pi-extensions/web-search.js";
+export { createSkillTools, listSkillFiles, loadSkillFile } from "./pi-extensions/skills.js";
+export type { SkillFileInfo, SkillToolOptions } from "./pi-extensions/skills.js";
 export * from "./llm/index.js";
